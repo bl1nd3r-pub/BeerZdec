ff934d328a1669e3e27ba83ef7b8c5f76cd25647
babikovap <babikovap@mer.ci.nsu.ru>
2026-05-20 13:17:48 +0700

    Фиксы страницы "О программе" - Пофиксил кодировку - Настроил чтение из файла - Перенёс файл в папку Resources

---
+++ Services/AppInfoService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BeerZdec.Services
+{
+    public class AppInfoService : IAppInfoService
+    {
+        private readonly string _filePath;
+
+        public AppInfoService()
+        {
+            // Путь к файлу в папке приложения
+            _filePath = Path.Combine(AppContext.BaseDirectory, "Resources", "about.txt");
+        }
+
+        public string GetAboutText()
+        {
+            if (File.Exists(_filePath))
+            {
+                return File.ReadAllText(_filePath, Encoding.UTF8);
+            }
+
+            return "Информация о приложении недоступна";
+        }
+    }
+}
